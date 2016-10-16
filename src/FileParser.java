b32c392fa1d4c521bc82067e0d9fd6788c6eaf44
Akshay <kumarsharmaa69@gmail.com>
2016-10-16 20:49:48 +0530

    change the parameter name from fileName to filePath in FileParser

--- src/FileParser.java
+++ src/FileParser.java
@@ -6,16 +6,15 @@ import java.io.File;
 import java.io.IOException;
 
 public class FileParser {
+    private String filePath;
 
-    private String fileName;
-
-    public FileParser(String fileName) {
-        this.fileName = fileName;
+    public FileParser(String filePath) {
+        this.filePath = filePath;
     }
 
 
     public JSONArray parse() throws IOException, JSONException {
-        File file = new File(fileName);
+        File file = new File(filePath);
         String fileData = FileUtils.readFileToString(file);
         JSONArray data = new JSONArray(fileData);
 
